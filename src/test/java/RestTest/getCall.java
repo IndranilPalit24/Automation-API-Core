a7e7b881e4b8a5da202fd475ed03612c23e91d16
Indranil <indronilpalit2013@gmail.com>
2025-07-09 21:00:46 +0530

    Changes to getCall Method

--- src/test/java/RestTest/getCall.java
+++ src/test/java/RestTest/getCall.java
@@ -2,28 +2,53 @@ package RestTest;
 
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 //static imports
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class getCall {
-	
+	/*
+	 * given():
+	 *   - Set content type
+	 *   - Set cookies
+	 *   - Add authentication
+	 *   - Add query or path parameters
+	 *   - Set headers
+	 *
+	 * when():
+	 *   - Perform HTTP method calls: GET, POST, PUT, DELETE, etc.
+	 *
+	 * then():
+	 *   - Validate status code
+	 *   - Extract response body, headers, cookies
+	 *   - Validate response body content
+	 */
+	@SuppressWarnings("deprecation")
 	@Test
 	public void getTeamDetails() {
-		given().
-		when().
-			get("https://www.thesportsdb.com/api/v1/json/123/searchteams.php?t=Arsenal").
-		then().
-			assertThat().
-			statusCode(200).
-			
-			and().
-			
-			body("teams.strTeam",contains("Arsenal")).
-			
-			and().
-			
-			header("Content-Length", equalTo("7942"));
+		try {
+			given().
+				//queryParam("t", "Arsenal").
+			when().
+				get("https://www.thesportsdb.com/api/v1/json/123/searchteams.php?t=Barcelona").
+			then().
+				assertThat().
+				statusCode(200).
+				
+				and().
+				body("teams.strTeam",contains("Barcelona", "FC Barcelona Basquet")).
+				
+				and().
+				header("Content-Length", equalTo("7941")).
+				log().all();    //log().all() -  presents the complete body of the response in console output
+		}
+		catch(Exception e) {
+			Assert.fail("Test Failed due to Exception "+e.getMessage());
+		}
 	}
+	
+	
 
 }
